10fef89bea777f427802883dccc8ff1d1f019801
Mason Smith <masonium@gmail.com>
2016-11-02 01:44:02 -0700

    doc update

--- rustdoc/implementors/ndarray/data_traits/trait.DataShared.js
+++ rustdoc/implementors/ndarray/data_traits/trait.DataShared.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["linxal"] = [];implementors["drav"] = [];
+implementors["linxal"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
